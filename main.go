2e27cceae51a16e4d17b87fb4d9c609e80f08ab6
unknown <agustiin98@gmail.com>
2021-07-06 20:22:25 -0300

    spacesHipAsteroids

--- main.go
+++ main.go
@@ -23,6 +23,7 @@ func run() error {
 	r := mux.NewRouter()
 	r.HandleFunc("/topsecret", h.GetSpaceshipCoordinates()).Methods(http.MethodPost)
 	r.HandleFunc("/topsecret_split/{satteliteName}", nil).Methods(http.MethodPost)
+	r.HandleFunc("/topsecret_split/{satteliteName}", nil).Methods(http.MethodGet)
 
 	log.Println("Listening on port 8080")
 	return http.ListenAndServe(":8080", r)
